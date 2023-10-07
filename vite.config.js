62309a65d438e32cc8d528960588a449caf0be21
matrix-man-12 <yogeshyadav32321212@gmail.com>
2023-10-08 02:52:57 +0530

    added routing and dummy pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,38 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
+  server:{
+    port: 3000,
+  },
+  // rollupOptions: {
+  //   external: ['react', 'react-dom'],
+  //   output: {
+  //     globals: {
+  //       react: 'React',
+  //     },
+  //   },
+  // },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
 })
