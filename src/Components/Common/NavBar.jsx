6baf991bef242907a65d454402d79516d97ec13b
matrix-man-12 <yogeshyadav32321212@gmail.com>
2023-10-15 20:56:19 +0530

    added navbar

---
+++ src/Components/Common/NavBar.jsx
@@ -0,0 +1,111 @@
+import React, {useState} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+import Constants from '../../Constants';
+
+const NavBar = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const pages = Constants.pages
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" style={{background: '#F5F5F5'}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Link to='/' className='link'>
+            <Typography
+              variant="h6"
+              noWrap
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+              }}
+            >
+              Demo
+            </Typography>
+          </Link>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((pageItem) => (
+              <span onClick={handleCloseNavMenu} className='space'>
+                <Link key={pageItem.id} to={pageItem.link} className='link'>                
+                  {pageItem.page}
+                </Link>
+              </span>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color='#f4f4f4'>
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((pageItem) => (
+                <Link key={pageItem.id} to={pageItem.link} className='link'>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{pageItem.page}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+
+          <Link to='/' className='link navlink'>
+            <Typography
+              variant="h6"
+              noWrap
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+                color: 'inherit',
+              }}
+            >
+              Demo
+            </Typography>
+          </Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default NavBar;
---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+export default {
+ pages: [
+    {id:'1', page: 'Home', link: '/home' }, 
+    {id:'2', page: 'Volunteer', link: '/join' }, 
+    {id:'3', page: 'Our Work', link: '/work' }, 
+    {id:'4', page: 'Know Us', link: '/about-us' }, 
+    {id:'5', page: 'Get In Touch', link: '/contact-us' }],
+ }
